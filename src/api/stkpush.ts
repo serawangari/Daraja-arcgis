9a7abec3aee33e5f388d124cd5c915c97137b8a8
serawangari <148107266+serawangari@users.noreply.github.com>
2025-05-23 11:22:02 +0300

    Updated stkpush.ts

--- src/api/stkpush.ts
+++ src/api/stkpush.ts
@@ -36,6 +36,17 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     // Get access token first
     const accessToken = await getAccessToken();
 
+    // Get env variables
+    const shortcode = process.env.DARAJA_BUSINESS_SHORT_CODE!;
+    const passkey = process.env.DARAJA_BUSINESS_SHORT_CODE_PASSKEY!;
+    const baseUrl = process.env.BASE_URL!;
+
+    // Generate EAT timestamp ONCE
+    const timestamp = getEATTimestamp();
+
+    // Generate password
+    const password = Buffer.from(`${shortcode}${passkey}${timestamp}`).toString('base64');
+
     // Define your payload
     const payload: STKPayload = {
       BusinessShortCode: shortcode,
