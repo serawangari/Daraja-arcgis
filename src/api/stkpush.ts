249c18ebc06b40c87213808bdabc1b4f4e0d07f7
serawangari <148107266+serawangari@users.noreply.github.com>
2025-05-23 11:48:36 +0300

    Update stkpush.ts

--- src/api/stkpush.ts
+++ src/api/stkpush.ts
@@ -40,6 +40,9 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     const shortcode = process.env.DARAJA_BUSINESS_SHORT_CODE!;
     const passkey = process.env.DARAJA_BUSINESS_SHORT_CODE_PASSKEY!;
     const baseUrl = process.env.BASE_URL!;
+    if  (!passkey) {
+      console.error("❌ DARAJA_BUSINESS_SHORT_CODE_PASSKEY is undefined!");
+    }
 
     // Generate EAT timestamp ONCE
     const timestamp = getEATTimestamp();
