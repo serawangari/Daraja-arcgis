98eba9d31a9ee77c1cfa4aa1ee0a875ff434fde4
serawangari <148107266+serawangari@users.noreply.github.com>
2025-05-23 11:10:06 +0300

    Update stkpush.ts

--- src/api/stkpush.ts
+++ src/api/stkpush.ts
@@ -38,7 +38,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
     // Get env variables
     const shortcode = process.env.DARAJA_BUSINESS_SHORT_CODE!;
-    const passkey = process.env.DARAJA_SHORT_CODE_PASSKEY!;
+    const passkey = process.env.DARAJA_BUSINESS_SHORT_CODE_PASSKEY!;
     const baseUrl = process.env.BASE_URL!;
     if (!passkey) {
        console.error("❌ DARAJA_SHORT_CODE_PASSKEY is undefined!");
